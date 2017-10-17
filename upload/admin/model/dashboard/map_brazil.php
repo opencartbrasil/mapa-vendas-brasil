0f092f417e8400375e71f01b43063c83c9767d6e
Manoel Vidal <manoel.vidal@opencartbrasil.com.br>
2017-10-17 14:44:59 -0200

    Corrigindo

--- upload/admin/model/dashboard/map_brazil.php
+++ upload/admin/model/dashboard/map_brazil.php
@@ -1,11 +1,7 @@
 <?php
 class ModelDashboardMapBrazil extends Model {
     public function getTotalOrdersByZone() {
-        if (version_compare(VERSION, '2.2') < 0) {
-            $query = $this->db->query("SELECT COUNT(*) AS total, SUM(o.total) AS amount, z.code as code FROM `" . DB_PREFIX . "order` o LEFT JOIN `" . DB_PREFIX . "zone` z ON (o.payment_zone_id = z.zone_id) WHERE o.order_status_id > '0' AND o.payment_country_id = '30' GROUP BY o.payment_zone_id");
-        } else {
-            $query = $this->db->query("SELECT COUNT(*) AS total, SUM(o.total) AS amount, c.iso_code_2 as code FROM `" . DB_PREFIX . "order` o LEFT JOIN `" . DB_PREFIX . "country` c ON (o.payment_country_id = c.country_id) WHERE o.order_status_id > '0' AND o.payment_country_id = '30' GROUP BY o.payment_country_id");
-        }
+        $query = $this->db->query("SELECT COUNT(*) AS total, SUM(o.total) AS amount, z.code as code FROM `" . DB_PREFIX . "order` o LEFT JOIN `" . DB_PREFIX . "zone` z ON (o.payment_zone_id = z.zone_id) WHERE o.order_status_id > '0' AND o.payment_country_id = '30' GROUP BY o.payment_zone_id");
 
         return $query->rows;
     }
